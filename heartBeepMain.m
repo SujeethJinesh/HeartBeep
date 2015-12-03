bf1799fb695c7f02c23788594c92a59e17886ece
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2015-12-02 23:58:55 -0500

    Update heartBeepMain.m

--- heartBeepMain.m
+++ heartBeepMain.m
@@ -8,18 +8,8 @@ function [str] = heartBeepMain(healthy,user,alpha)
     
     log = true; %change later
     [time col2 col3] = getData(healthy); % getting data from text file
-    
-    subplot(2,1,1)
-    
+
     plot3(time, col2, col3);
-   
-    Y1 = fft(col2);
-    
-    Y2 = fft(col3);
-    
-    subplot(2,1,2)
-    
-    plot3(time,Y1, Y2);
     
     str = analyzeData(time, col2, col3); % determining if person has disease
     % str = confidenceData(time,col2,col3,alpha) cofidence level, do we
