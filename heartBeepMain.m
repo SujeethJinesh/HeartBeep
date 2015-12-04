bff42f519726028f3efdf9a0ffe9c8ca7c9fc81e
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2015-12-04 04:47:53 -0500

    Update heartBeepMain.m

--- heartBeepMain.m
+++ heartBeepMain.m
@@ -4,15 +4,14 @@ function [str] = heartBeepMain(healthy,user,alpha)
     % alpha is confidence level requested
     % KEEP FOR NOW log is if the patient is healthy or not. True is healthy false is
     % diseased
-    % str gives cofidence 
+    % str gives confidence 
     
-    log = true; %change later
     [time col2 col3] = getData(healthy); % getting data from text file
 
     plot3(time, col2, col3);
     
-    str = analyzeData(time, col2, col3); % determining if person has disease
-    % str = confidenceData(time,col2,col3,alpha) cofidence level, do we
-    % need this function?
-end
+    [str, idMin, idMax] = analyzeData(time, col2, col3, alpha); % determining if person has disease
     
+    %plays the heartbeat sound
+    playSound(user, idMin, idMax);
+end
